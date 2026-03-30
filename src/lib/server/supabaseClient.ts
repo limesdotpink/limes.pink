9fb1f6647e110dfdd40f7af9ae17bc0826a2def8
limes <git@limes.pink>
2026-03-30 17:41:20 +0200

    feat: containerize

--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -1,14 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-import { SUPABASE_URL, SUPABASE_SERVICE_KEY } from '$env/static/private';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseServerKey = process.env.SUPABASE_SERVICE_KEY || '';
 
-const supabaseUrl = SUPABASE_URL || '';
-const supabaseServerKey = SUPABASE_SERVICE_KEY || '';
-
-export let supabase;
-
-if (process.env.NODE_ENV?.toString().includes('dev')) {
-	supabase = undefined;
-} else {
+let supabase;
+if (supabaseUrl && supabaseServerKey) {
 	supabase = createClient(supabaseUrl, supabaseServerKey);
 }
+
+export { supabase };
