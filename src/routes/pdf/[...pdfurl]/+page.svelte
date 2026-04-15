23ae3e8fafddd0bf4f4ad4c3aad730a10b04b0a1
limes <git@limes.pink>
2026-04-15 03:28:38 +0200

    feat: clarify that I don't endorse the content in the viewer

---
+++ src/routes/pdf/[...pdfurl]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+    const gDocsUrl = `https://docs.google.com/gview?url=${encodeURIComponent(`${page.params.pdfurl}${page.url.search}`)}&embedded=true`;
+</script>
+
+<div class="wrapper">
+<p>This is merely a PDF viewer. limes.pink does not control/endorse whatever content you're seeing.</p>
+<iframe src="{gDocsUrl}" width="100%" height="100%" title="Google Docs PDF viewer" frameborder="0"></iframe>
+</div>
+
+<style>
+    .wrapper {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-rows: fit-content(100%) auto;
+    }
+
+    p {
+        display: block;
+        width: 100%;
+        background-color: var(--pink);
+        margin: 0;
+        height: fit-content;
+        padding: .25rem;
+        text-align: center;
+    }
+</style>
