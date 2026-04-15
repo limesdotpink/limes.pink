2900797498741d85482c67c0aca4bd1bdf07d5d5
limes <git@limes.pink>
2026-04-15 16:31:24 +0200

    feat(pdf): add sandbox attribute to iframe, clean up meta tags

--- src/routes/pdf/[...pdfurl]/+page.svelte
+++ src/routes/pdf/[...pdfurl]/+page.svelte
@@ -9,27 +9,68 @@
 	let userIsAware = $derived(data.pdfUserIsAware);
 	let doNotShowAgain = $state(false);
 
+	const pdfNameRegex = /[^/]+(?=\/$|$)/;
+	const pdfName = pdfNameRegex.exec(page.params.pdfurl!)![0];
+
+	const pageTitle = [pdfName, 'PDF viewer'].join(' | ');
+
 	const gDocsUrl = `https://docs.google.com/gview?url=${encodeURIComponent(`${page.params.pdfurl}${page.url.search}`)}&embedded=true`;
 </script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+	<meta property="og:title" content={pageTitle} />
+	<meta property="twitter:title" content={pageTitle} />
+	<meta
+		name="description"
+		content="The Google Drive PDF viewer, embedded for your convenience. limes.pink does not control, endorse, or host the embedded file, which may contain sensitive content. Only proceed if you trust the person who sent you this. If you did not expect this file, do not open it."
+	/>
+	<meta
+		property="twitter:description"
+		content="The Google Drive PDF viewer, embedded for your convenience. limes.pink does not control, endorse, or host the embedded file, which may contain sensitive content. Only proceed if you trust the person who sent you this. If you did not expect this file, do not open it."
+	/>
+	<meta
+		property="og:description"
+		content="The Google Drive PDF viewer, embedded for your convenience. limes.pink does not control, endorse, or host the embedded file, which may contain sensitive content. Only proceed if you trust the person who sent you this. If you did not expect this file, do not open it."
+	/>
+</svelte:head>
 
 {#if userIsAware}
 	<div class="wrapper">
 		<p>
-			This is merely a PDF viewer. limes.pink does not control/endorse whatever content you're
-			seeing.
+			This is merely a PDF viewer. <b
+				>limes.pink does not control, endorse, or host the embedded file.</b
+			>
 		</p>
-		<iframe src={gDocsUrl} width="100%" height="100%" title="Google Docs PDF viewer" frameborder="0"
+		<iframe
+			src={gDocsUrl}
+			width="100%"
+			height="100%"
+			referrerpolicy="no-referrer"
+			sandbox="allow-scripts allow-same-origin allow-popups"
+			title="Google Drive PDF viewer"
+			frameborder="0"
 		></iframe>
 	</div>
 {:else}
 	<div class="curtain">
 		<div class="notice">
-			<h1>WARNING!</h1>
+			<h1><span>WARNING!</span></h1>
 			<p>
-				The page you're about to visit is merely a PDF viewer, and may contain sensitive content.
+				The page you're about to visit is merely a PDF viewer, which displays a file hosted on a
+				third-party site.
 			</p>
-			<p>limes.pink does not control/endorse the content embedded in this page.</p>
-			<p>Make sure you trust the person who sent you to this page.</p>
+			<p>
+				<span
+					>limes.pink does not control, endorse, or host the embedded file, which may contain
+					sensitive content.</span
+				>
+			</p>
+			<p>
+				Only proceed if you trust the person who sent you to this page. <span
+					>If you did not expect this file, do not open it.</span
+				>
+			</p>
 		</div>
 		<Button
 			onclick={() => {
@@ -38,7 +79,7 @@
 				}
 
 				userIsAware = true;
-			}}>I understand, take me there</Button
+			}}>I understand, take me there.</Button
 		>
 		<label class={{ checked: doNotShowAgain }}>
 			<input type="checkbox" bind:checked={doNotShowAgain} />
@@ -60,21 +101,23 @@
 		display: flex;
 		flex-flow: column nowrap;
 		justify-content: center;
+		align-items: center;
 	}
 
 	.curtain p {
-		margin: 0 0 0.25rem;
+		margin: 0 0 0.5rem;
 	}
 
-	.curtain .notice {
-		margin-bottom: 1.5rem;
-		padding: 0 1rem;
+	.curtain span {
+		background: var(--pink);
+		color: var(--bg-pink);
+		font-weight: 700;
 	}
-
-	.curtain .notice h1 {
-		margin: 0.5rem 0;
+	.curtain :global(button) {
+		font-weight: 700;
+		width: fit-content;
+		color: var(--bg-pink);
 	}
-
 	.curtain label {
 		display: flex;
 		color: var(--text-muted);
@@ -83,7 +126,22 @@
 		gap: 0.25rem;
 		margin-top: 0.5rem;
 		transition: color 200ms;
+		font-size: 0.8em;
 	}
+	.curtain :global(button):hover,
+	.curtain label:hover {
+		text-decoration: underline;
+	}
+
+	.curtain .notice {
+		margin-bottom: 2rem;
+		padding: 0 1rem;
+	}
+
+	.curtain .notice h1 {
+		margin: 2rem 0;
+	}
+
 	.curtain label.checked {
 		color: unset;
 	}
