84892c79db8a09170db1be1e65c4aabe70a76e1c
limes <git@limes.pink>
2026-04-21 15:56:43 +0200

    feat: tentatively add cors headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,17 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
-const handleParaglide: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request, locale }) => {
+const handleParaglide: Handle = async ({ event, resolve }) =>
+	await paraglideMiddleware(event.request, async ({ request, locale }) => {
 		event.request = request;
 
-		return resolve(event, {
+		const response = await resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
 		});
+
+		response.headers.append('Access-Control-Allow-Origin', `*.limes.pink`);
+
+		return response;
 	});
 
 export const handle: Handle = handleParaglide;
