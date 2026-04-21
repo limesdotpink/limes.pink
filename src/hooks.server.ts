01ce03703e7f21af4c2bf461bf2ebc95fbbfb34b
limes <git@limes.pink>
2026-04-21 16:11:45 +0200

    feat: tentatively add dynamic cors header

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,23 @@
 import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
-const handleParaglide: Handle = async ({ event, resolve }) =>
-	await paraglideMiddleware(event.request, async ({ request, locale }) => {
+export const handle: Handle = async ({ event, resolve }) => {
+	const response = await paraglideMiddleware(event.request, async ({ request, locale }) => {
 		event.request = request;
 
-		const response = await resolve(event, {
+		return await resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
 		});
-
-		response.headers.append('Access-Control-Allow-Origin', `*.limes.pink`);
-
-		return response;
 	});
 
-export const handle: Handle = handleParaglide;
+	const corsDomainRegex = /^(.*\.)?limes\.pink$/;
+
+	console.log(event.request.headers.get('origin'))
+	const origin = new URL(event.request.headers.get('origin') || 'https://example.org').hostname;
+
+	if (corsDomainRegex.test(origin)) {
+		response.headers.append('Access-Control-Allow-Origin', `https://${origin}`);
+	}
+
+	return response;
+};
