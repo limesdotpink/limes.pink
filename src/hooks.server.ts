8422162c2d701e2ab24835a4306aa6de84b5704f
limes <git@limes.pink>
2026-04-28 23:43:43 +0200

    fix: cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,10 +12,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	const corsDomainRegex = /^(.*\.)?limes\.pink$/;
 
-	console.log(event.request.headers.get('origin'))
-	const origin = new URL(event.request.headers.get('origin') || 'https://example.org').hostname;
+	const host = event.request.headers.get('host') || new URL(event.request.headers.get('origin') || 'https://example.org').hostname;
 
-	if (corsDomainRegex.test(origin)) {
+	if (corsDomainRegex.test(host)) {
 		response.headers.append('Access-Control-Allow-Origin', `https://${origin}`);
 	}
 
